b57ba6175fb277fb2ed18e6ff94c8a10fdc26659
shimobayashi <akimasashimobayashi@gmail.com>
2020-01-01 12:03:48 +0900

    なんか動いてなかったっぽいので色々と修正した。

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,43 +1,54 @@
 require 'json'
 require 'net/https'
 require 'uri'
+require 'logger'
+
+$logger = Logger.new(STDOUT)
+$logger.level = ENV['LOG_LEVEL'] || 'FATAL'
 
 def lambda_handler(event:, context:)
-  if (event.has_key?('body'))
-    json = JSON.parse(event['body'])
+  $logger.debug(event)
 
-    case json['alert']['monitorName']
+  body = 'Nothing to do'
+  if (event.has_key?('alert'))
+    case event['alert']['monitorName']
     when 'DiscomfortIndex(TooCold)' then
-      case json['alert']['status']
+      case event['alert']['status']
       when 'critical'
-        aircon_on_heater
+        body = aircon_on_heater
       when 'ok'
-        aircon_off
+        body = aircon_off
       end
     when 'DiscomfortIndex(TooHot)' then
-      case json['alert']['status']
+      case event['alert']['status']
       when 'critical'
-        aircon_on_cooler
+        body = aircon_on_cooler
       when 'ok'
-        aircon_off
+        body = aircon_off
       end
     end
   end
 
-  { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
+  { statusCode: 200, body: JSON.generate(body) }
 end
 
 def aircon_on_heater
-  puts 'heater on'
-  puts Net::HTTP.get(URI.parse("https://maker.ifttt.com/trigger/aircon_on_heater/with/key/#{ ENV['IFTTT_API_KEY'] }"))
+  $logger.info('heater on')
+  res = Net::HTTP.get(URI.parse("https://maker.ifttt.com/trigger/aircon_on_heater/with/key/#{ ENV['IFTTT_API_KEY'] }"))
+  $logger.info(res)
+  return res.to_s
 end
 
 def aircon_on_cooler
-  puts 'cooler on'
-  puts Net::HTTP.get(URI.parse("https://maker.ifttt.com/trigger/aircon_on_cooler/with/key/#{ ENV['IFTTT_API_KEY'] }"))
+  $logger.info('cooler on')
+  res = Net::HTTP.get(URI.parse("https://maker.ifttt.com/trigger/aircon_on_cooler/with/key/#{ ENV['IFTTT_API_KEY'] }"))
+  $logger.info(res)
+  return res.to_s
 end
 
 def aircon_off
-  puts 'aircon off'
-  puts Net::HTTP.get(URI.parse("https://maker.ifttt.com/trigger/aircon_off/with/key/#{ ENV['IFTTT_API_KEY'] }"))
+  $logger.info('aircon off')
+  res = Net::HTTP.get(URI.parse("https://maker.ifttt.com/trigger/aircon_off/with/key/#{ ENV['IFTTT_API_KEY'] }"))
+  $logger.info(res)
+  return res.to_s
 end

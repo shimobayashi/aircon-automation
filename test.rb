aadeae9cdb4541b5b1602475a53a9aa13d353f1f
shimobayashi <akimasashimobayashi@gmail.com>
2020-06-28 15:24:26 +0900

    テスト書かないと保守不能になってきたのでテストの雛形を書いた。

---
+++ test.rb
@@ -0,0 +1,14 @@
+require 'test/unit'
+require './lambda_function'
+
+class TC_LambdaFunction < Test::Unit::TestCase
+    def setup
+    end
+
+    def teardown
+    end
+
+    def test_lambda_function
+        assert_equal(1, 1)
+    end
+end

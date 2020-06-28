f792b61d99f43d7de76c1b3031c4ffa3c3081b45
shimobayashi <akimasashimobayashi@gmail.com>
2020-06-28 17:49:05 +0900

    ちゃんとどのEventを呼び出してるかもチェックするようにした。そして元のテストケースがコピペミスしてることに気づいた。今日はもうコード書きたくない……。

--- test.rb
+++ test.rb
@@ -3,7 +3,8 @@ require './lambda_function'
 
 Net::HTTP.class_eval do
     def self.get(uri)
-        return "Congratulations! You've fired the aircon_on_cooler event"
+        event_name = uri.path[/^\/trigger\/(.+?)\/with\/key\//, 1]
+        return "Congratulations! You've fired the #{event_name} event"
     end
 end
 
@@ -19,6 +20,6 @@ class TC_LambdaFunction < Test::Unit::TestCase
         # 動作に必要ない情報もコピペで色々ぶち込んであるので、そのうちmetricLabelとかが現実と乖離するかも知れない。
         event = {"orgName"=>"shimobayashi", "alert"=>{"monitorName"=>"DiscomfortIndex(TooHot)", "criticalThreshold"=>79.25, "metricValue"=>75.628, "monitorOperator"=>">", "trigger"=>"monitor", "warningThreshold"=>77.25, "url"=>"https://mackerel.io/orgs/shimobayashi/alerts/3XUWssb2vD3", "openedAt"=>1593273842, "duration"=>1, "createdAt"=>1593273842902, "isOpen"=>false, "metricLabel"=>"alias(\n    offset(\n        sum(\n            group(\n                scale(service(home, natureremo.temperature.Remo), 0.81),\n                scale(product(group(offset(scale(service(home, natureremo.temperature.Remo), 0.99), -14.3), service(home, natureremo.humidity.Remo))), 0.01)\n            )\n        ), 46.3\n    ), 'discomfort-index'\n)", "id"=>"3XUWssb2vD3", "closedAt"=>1593277142, "status"=>"ok"}, "event"=>"alert", "user"=>nil}
         response = lambda_handler(event: event, context: {})
-        assert_equal({ statusCode: 200, body: "\"Congratulations! You've fired the aircon_on_cooler event\"" }, response)
+        assert_equal({ statusCode: 200, body: "\"Congratulations! You've fired the aircon_off event\"" }, response)
     end
 end
